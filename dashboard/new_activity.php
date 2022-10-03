8f2e7f26b109ad74f5a863c181b2a6bdaa268140
oratecjamshoro <68347990+alijanrahujo@users.noreply.github.com>
2022-10-03 14:05:02 +0500

    update

--- dashboard/new_activity.php
+++ dashboard/new_activity.php
@@ -9,9 +9,9 @@ include("head.php");
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
-            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Transition History</li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">New Activity</li>
           </ol>
-          <h6 class="font-weight-bolder mb-0">Transition History</h6>
+          <h6 class="font-weight-bolder mb-0">New Activity</h6>
         </nav>
         <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
@@ -113,7 +113,7 @@ include("head.php");
     <!-- End Navbar -->
     <div class="container-fluid py-4">
       
-      <h3>Transition History</h3>
+      <h3>New Activity</h3>
       <div class="row my-4">
         <div class="col-lg-12 col-md-12 mb-md-0 mb-4">
           <div class="card">
